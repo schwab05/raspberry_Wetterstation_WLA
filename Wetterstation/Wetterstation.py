7aa22c607e399ec25e2ac87fdf940e1d9f7ad45f
Markus-Auer <45470745+Markus-Auer@users.noreply.github.com>
2019-10-21 17:17:54 +0200

    adfa

--- Wetterstation/Wetterstation.py
+++ Wetterstation/Wetterstation.py
@@ -1,3 +1,24 @@
+import RPi.GPIO as GPIO
+
 temp = 0
 preassure = 0
 humidity = 0
+buttonpin = 17
+ledpin = 18
+
+
+reset = False
+refresh = False
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(buttonpin, GPIO.IN)
+GPIO.setup(ledpin, GPIO.OUT)
+
+while True:
+    if  GPIO.input(buttonpin) == 1:
+        GPIO.output(ledpin, GPIO.HIGH)
+
+
